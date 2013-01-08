192870b840243eb79961bcd3256f9abd67a3c082
andrew <ahlavats@rogers.com>
2013-01-07 21:03:45 -0500

    height and length of game array verified

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -5,10 +5,17 @@ describe Game do
   subject(:game) { Game.create(11, 10) }
   let(:session) { Hash.new }
 
-  it "should initialize" do
-    session = Hash.new
+  it "should have the specified height" do
     game.save(session)
 
     session[:board].length.should == 10
   end
+
+  it "should have the specified length" do
+    game.save(session)
+
+    session[:board][0].length.should == 11
+  end
+
+
 end
