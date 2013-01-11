4517e6c50e1246da38826017331d33fe7c76b6ee
Pete Bevin <pete@petebevin.com>
2013-01-11 08:25:14 -0500

    Remove db migrations for unused tables

--- db/migrate/20130108005233_create_sessions.rb
+++
@@ -1,9 +0,0 @@
-class CreateSessions < ActiveRecord::Migration
-  def change
-    create_table :sessions do |t|
-      t.integer :game_id
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130108005233) do
-
-  create_table "boards", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "games", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "sessions", :force => true do |t|
-    t.integer  "game_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 0) do
 
 end
