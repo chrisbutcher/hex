3b66615ec4baa026bcfce29fd4b2c8ee8df18388
Pete Bevin <pete@petebevin.com>
2013-01-26 13:40:46 -0500

    Split out Hex::Board type

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ActiveRecord::Base
   def self.start(width, height)
     Game.create do |game|
-      game.board_state = Board.create(width, height).state
+      game.board_state = Hex::Board.create(width, height).state
     end
   end
 
@@ -14,31 +14,6 @@ class Game < ActiveRecord::Base
   end
 
   def board
-    Board.new(board_state)
-  end
-
-
-  class Board
-    def initialize(tiles)
-      @tiles = tiles
-    end
-
-    def self.create(width, height)
-      row = "." * width + "\n"
-      tiles = row * height
-      Board.new(tiles)
-    end
-
-    def state
-      @tiles
-    end
-
-    def width
-      @tiles.split.first.strip.length
-    end
-
-    def height
-      @tiles.split.length
-    end
+    Hex::Board.new(board_state)
   end
 end
